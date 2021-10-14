8d058fceaedcbc518c42276c5c230eff9a0956b2
Anish Vijay <anishvijay980@gmail.com>
2021-10-14 23:09:42 +0530

    VideoPlayer

--- script.js
+++ script.js
@@ -1,120 +1,68 @@
 // Global Variables
 let zerosCrosses = Array.from(document.querySelectorAll(".child"));
 let zeroAndCross = Array.from(document.querySelectorAll(".selector"));
+let cross = Array.from(document.querySelectorAll(".zr"));
+cross.push(document.querySelector(".crx"))
 let selectionBox = document.querySelector(".player1");
 let h1 = document.querySelector("h1");
 let body = document.querySelector("body");
 let player1;
 let ox = [];
-
+let winsComb = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 let a = 0;
 
 let textinspect = [];
 let oOrC = [];
 
+cross.forEach(item => {
+    item.addEventListener("click", () => {
+        let audio = new Audio("8_bit Surf.mp3");
+        audio.loop = true;
+    })
+})
+
 function ifStatement(f1, s1, s2) {
     return (
         zerosCrosses[f1].innerText === zerosCrosses[s1].innerText &&
-        zerosCrosses[f1].innerText === zerosCrosses[s2].innerText
+        zerosCrosses[f1].innerText === zerosCrosses[s2].innerText &&
+        zerosCrosses[f1].innerText !== ""
     );
 }
 
-function color(n1, n2, n3) {
+function color(n1, n2, n3, ...theArgs) {
     zerosCrosses[n1].style.color = "lightgreen"
     zerosCrosses[n2].style.color = "lightgreen"
     zerosCrosses[n3].style.color = "lightgreen";
 }
 
+function matchText(elem) {
+    return (setTimeout(() => {
+        h1.innerText = zerosCrosses[elem].innerText;
+        h1.innerText += " Won";
+    }, 150))
+}
 let condition_check = false;
 
-function IfElse() {
-    if (oOrC.includes(0) && oOrC.includes(1) && oOrC.includes(2)) {
-        if (ifStatement(0, 1, 2)) {
-            color(0, 1, 2)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[0].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
+function winnerDecider() {
+    winsComb.forEach((element) => {
+        if (oOrC.includes(element[0]) && oOrC.includes(element[1]) && oOrC.includes(element[2])) {
+            if (ifStatement(element[0], element[1], element[2])) {
+                matchText(element[0]);
+                color(element[0], element[1], element[2])
+                condition_check = true;
+            }
         }
-    }
-    if (oOrC.includes(3) && oOrC.includes(4) && oOrC.includes(5)) {
-        if (ifStatement(3, 4, 5)) {
-            color(3, 4, 5)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[3].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
+    })
 
-        }
-    }
-    if (oOrC.includes(6) && oOrC.includes(7) && oOrC.includes(8)) {
-        if (ifStatement(6, 7, 8)) {
-            color(6, 7, 8)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[6].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
-    if (oOrC.includes(0) && oOrC.includes(4) && oOrC.includes(8)) {
-        if (ifStatement(0, 4, 8)) {
-            color(0, 4, 8)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[0].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
-    if (oOrC.includes(2) && oOrC.includes(4) && oOrC.includes(6)) {
-        if (ifStatement(2, 4, 6)) {
-            color(2, 4, 6)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[2].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
-    if (oOrC.includes(0) && oOrC.includes(3) && oOrC.includes(6)) {
-        if (ifStatement(0, 3, 6)) {
-            color(0, 3, 6)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[0].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
-    if (oOrC.includes(1) && oOrC.includes(4) && oOrC.includes(7)) {
-        if (ifStatement(1, 4, 7)) {
-            color(1, 4, 7)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[1].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
-    if (oOrC.includes(2) && oOrC.includes(5) && oOrC.includes(8)) {
-        if (ifStatement(2, 5, 8)) {
-            color(2, 5, 8)
-            setTimeout(() => {
-                h1.innerText = zerosCrosses[2].innerText;
-                h1.innerText += " Won";
-            }, 150)
-            condition_check = true;
-
-        }
-    }
     return condition_check;
     // reset(condition_check);
 }
@@ -129,8 +77,9 @@ function reset() {
         resetBtn.classList.add("reset");
         resetBtn.setAttribute("type", "submit");
         resetBtn.innerText = "Reset";
-        let form = document.querySelector("form");
+        let form = document.querySelector(".form");
         form.appendChild(resetBtn)
+        resetBtn.style.display = "none"
     }, 400);
 
 }
@@ -162,16 +111,10 @@ function GameLoop() {
 
             for (let i = 0; i < zerosCrosses.length; i++) {
 
-                function audio() {
-                    let audio = new Audio("music.mp3");
-                    audio.play();
-                }
+
+
                 zerosCrosses[i].addEventListener("click", () => {
-                    let audio = new Audio("music.mp3");
-                    audio.play();
-                })
-                setInterval(audio(), 70000)
-                zerosCrosses[i].addEventListener("click", () => {
+
 
                     zerosCrosses[i].innerText = ox[a++];
                     zerosCrosses[i].style.pointerEvents = "none";
@@ -190,7 +133,7 @@ function GameLoop() {
                         oOrC.length == 9
                     ) {
                         setTimeout(() => {
-                            let a = IfElse();
+                            let a = winnerDecider();
 
                             if (oOrC.length == 9) {
                                 if (a != true) {
@@ -215,3 +158,69 @@ function GameLoop() {
     });
 }
 GameLoop();
+
+
+
+// if (oOrC.includes(0) && oOrC.includes(1) && oOrC.includes(2)) {
+//     if (ifStatement(0, 1, 2)) {
+//         color(0, 1, 2)
+//         matchText(0)
+//         condition_check = true;
+//     }
+// }
+// if (oOrC.includes(3) && oOrC.includes(4) && oOrC.includes(5)) {
+//     if (ifStatement(3, 4, 5)) {
+//         color(3, 4, 5)
+//         matchText(3)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(6) && oOrC.includes(7) && oOrC.includes(8)) {
+//     if (ifStatement(6, 7, 8)) {
+//         color(6, 7, 8)
+//         matchText(6)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(0) && oOrC.includes(4) && oOrC.includes(8)) {
+//     if (ifStatement(0, 4, 8)) {
+//         color(0, 4, 8)
+//         matchText(0)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(2) && oOrC.includes(4) && oOrC.includes(6)) {
+//     if (ifStatement(2, 4, 6)) {
+//         color(2, 4, 6)
+//         matchText(2)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(0) && oOrC.includes(3) && oOrC.includes(6)) {
+//     if (ifStatement(0, 3, 6)) {
+//         color(0, 3, 6)
+//         matchText(0)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(1) && oOrC.includes(4) && oOrC.includes(7)) {
+//     if (ifStatement(1, 4, 7)) {
+//         color(1, 4, 7)
+//         matchText(1)
+//         condition_check = true;
+
+//     }
+// }
+// if (oOrC.includes(2) && oOrC.includes(5) && oOrC.includes(8)) {
+//     if (ifStatement(2, 5, 8)) {
+//         color(2, 5, 8)
+//         matchText(2)
+//         condition_check = true;
+
+//     }
+// }
